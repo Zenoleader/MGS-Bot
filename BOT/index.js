8ccc49ee29c16f89cd348afa3dd3869b98a74dc3
Zeno <119907481+Zenoleader@users.noreply.github.com>
2023-01-03 07:20:43 -0600

    Update index.js

--- BOT/index.js
+++ BOT/index.js
@@ -18,41 +18,7 @@ client.on("messageCreate", message => {
  if(message.content === "!textcommand") {
   message.channel.send("text here")
   } 
- }
-})      
-
-//HELP
-client.on("messageCreate", message =>{
-if(message.content === "!embedcommand") {
-  let embed  = new Discord.MessageEmbed()
-  .setTitle("text")
-    .setAuthor("text")
-  .setDescription("text")
-      .addFields(
-        {
-        name: ';command',
-        value: 'text'
-        },
-        {
-          name: ';command',
-          value: 'text'
-        },
-)
-  .setColor("GREEN")
-  .setTimestamp()
-  message.channel.send({embeds: [embed]})
-}
- })
-
-//STATUS
-client.on("ready", () => {
-  console.log(`Bot has started, with ${client.users.cache.size} 
-  users, in ${client.channels.cache.size} channels of     
-  ${client.guilds.cache.size} guilds.`);
-
-client.on("ready", () => {
-client.user.setPresence({activities: [{name: 'text here', TYPE: 'PLAYING'}], status: 'dnd'});
-})
+ })      
 
 client
   .on("debug", console.log)
