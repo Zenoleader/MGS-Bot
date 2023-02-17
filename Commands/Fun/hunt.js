605da80a2f01fc8a70a68ef80f4a2417f5bb962b
Zeno <119907481+Zenoleader@users.noreply.github.com>
2023-02-16 21:06:49 -0600

    Create hunt.js

---
+++ Commands/Fun/hunt.js
@@ -0,0 +1,14 @@
+if(message.content === "!hunt") {
+   
+       var animals = [
+      '🐿 | Squirrel',
+      '🦇 | Bat ',
+      '🦝 | Raccoon',
+      '🦊 | Fox',
+      '🦫 | Beaver',
+      '🥫 | Can',
+      '💀 | Nothing.',
+      '🐻 | BEAR',
+    ]
+
+    message.channel.send(`${message.author.tag} got a ${animals[Math.floor(Math.random() * animals.length)]}`)
