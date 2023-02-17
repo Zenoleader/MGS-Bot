45f031a65c7eef9ad2af98fb2da3f27f02b5844f
Zeno <119907481+Zenoleader@users.noreply.github.com>
2023-02-17 14:33:20 -0600

    Create buttons.js

---
+++ Commands/Basic/buttons.js
@@ -0,0 +1,19 @@
+if(message.content === '!buttons') {
+ 
+  const row = new Discord.MessageActionRow()
+    .addComponents(
+          new Discord.MessageButton()
+    .setCustomId("primary")
+      .setLabel("Button 1")
+      .setStyle('SECONDARY')
+      
+      ,new Discord.MessageButton()
+        .setCustomId("secondary")
+      .setLabel("Button 2")
+      .setStyle('SECONDARY')
+    )
+  
+  message.channel.send({content: 'Buttons!', components: [row]})
+  
+  //Code button interactions down here 👍
+}
