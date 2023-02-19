60842b5809df47cc49ff076817635d1adb60c866
Zeno <119907481+Zenoleader@users.noreply.github.com>
2023-02-19 17:31:05 -0600

    Update Status & appearance.js

--- Commands/Basic/Status & appearance.js
+++ Commands/Basic/Status & appearance.js
@@ -5,6 +5,5 @@ client.on("ready", () => {
   users, in ${client.channels.cache.size} channels of     
   ${client.guilds.cache.size} guilds.`);
 
-client.on("ready", () => {
 client.user.setPresence({activities: [{name: 'text here', TYPE: 'PLAYING'}], status: 'dnd'});
 })
