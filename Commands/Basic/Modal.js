56d32962dadbe16eebe5ccbd5aecb836ea42327f
Zeno <119907481+Zenoleader@users.noreply.github.com>
2023-02-22 20:50:47 -0600

    Create Modal.js

---
+++ Commands/Basic/Modal.js
@@ -0,0 +1,45 @@
+if(message.content === "!modal") {
+ const modal = new Discord.MessageActionRow()
+    .addComponents(
+    new Discord.MessageButton()
+      .setCustomId('modal-button')
+      .setStyle('PRIMARY')
+      .setLabel('Modal :)') 
+      );
+  
+  message.reply({components: [modal]})
+}
+
+client.on('interactionCreate', async interaction => {
+  if (interaction.isButton()) {
+    if (interaction.customId === 'modal-button') {
+    const modal = new Discord.Modal()
+        .setCustomId('test-modal')
+        .setTitle('Modal test!')
+        .addComponents([
+          new Discord.MessageActionRow()
+          .addComponents(
+          new Discord.TextInputComponent()
+              .setCustomId('modal-input')
+              .setLabel('STill testing')
+              .setStyle('SHORT')
+              .setMinLength(1)
+              .setMaxLength(10)
+              .setPlaceholder('Say something')
+              .setRequired(true),
+          )
+        ]);
+      
+            await interaction.showModal(modal);
+    }
+    
+    if (!interaction.isModalSubmit()) return;
+  
+if (interaction.customId === 'test-modal') {
+  
+const response = interaction.fields.getTextInputValue('modal-input');
+  
+interaction.reply(`Got it: ${response}`);
+  
+    }
+    })
