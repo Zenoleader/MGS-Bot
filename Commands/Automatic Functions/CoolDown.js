bae0e7882e573d8976157e372abbd3497e972d6c
Zeno <119907481+Zenoleader@users.noreply.github.com>
2023-03-20 09:18:07 -0500

    Update CoolDown.js

--- Commands/Automatic Functions/CoolDown.js
+++ Commands/Automatic Functions/CoolDown.js
@@ -1,3 +1,5 @@
+//Note: There is a bug with the cooldown that it keeps saying you need to wait when you just send a normal message. We're working to fix.
+
 const cooldowns = new Map();
 
 if(message.content === "!cooldown") {
