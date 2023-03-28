af2719c1554502ae0d9b70643b812a32f934f88b
Zeno <119907481+Zenoleader@users.noreply.github.com>
2023-03-28 18:52:14 -0500

    Update ReportCommand.js

--- Commands/Moderation/ReportCommand.js
+++ Commands/Moderation/ReportCommand.js
@@ -5,6 +5,8 @@ if(message.content.startsWith('!report')) {
 
   const args = message.content.slice('!report'.length).trim().split(/ + /g);
   const response = args.join('');
+  //this channel ID would be the logging channel, or the channel you want to see reports
 client.channels.cache.get('CHANNEL_ID HERE').send(response+" :USER ID-"+message.author.id+" :SERVER ID-"+message.guild.id)
   message.channel.send('Sent!')
+  
 }
