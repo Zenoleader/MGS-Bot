06ee85e35dcc049017699a68e9108183dddea157
ZenoYT <87985541+ZenoYT@users.noreply.github.com>
2023-08-12 15:57:21 -0500

    Update balance.js

--- Command Handler src/commands/economy/balance.js
+++ Command Handler src/commands/economy/balance.js
@@ -1,1 +1,19 @@
-//hold
+const db = require('quick.db')
+
+module.exports = {
+  name: 'balance',
+  aliases: ['bal', 'bank'],
+  description: 'See the balance of you / other users!',
+  async execute(message) {
+
+const user = message.author;
+    const balance = await db.get(`balance_${user.id}`)
+    if (balance === null) {
+      await message.channel.send(`${user} doesn't have a balance.`)
+    } else {
+       const format = balance.toLocaleString()
+      await message.channel.send(`${user}'s balance is currently ${format} Cash!`)
+    }
+
+  }
+}
