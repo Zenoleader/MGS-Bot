32547cb6bcb95057f40b65824a81ad21516881f7
Zeno <119907481+Zenoleader@users.noreply.github.com>
2023-08-13 09:23:59 -0500

    Create ban.js

---
+++ Command Handler src/commands/moderation/ban.js
@@ -0,0 +1,20 @@
+module.exports = {
+  name: 'togglerob',
+  description: 'Toggle the rob command!',
+  async execute(message, args) {
+
+      const reason = args.join(''); 
+    if (!args[0]) return message.reply("You need to mention someone.")
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]);
+    if (!message.member.permissions.has("BAN_MEMBERS")) return message.channel.send("You don't have perms to use thins!");
+    if (!message.guild.me.permissions.has("BAN_MEMBERS")) return message.channel.send("I don't have permission to ban people!");
+    if (message.member.id === member.id) return message.channel.send("You can't ban yourself :\");
+
+    member.ban({ reason: reason })
+    
+    
+client.channels.cache.get('LOGS CHANNEL_ID HERE').send(`Banned user: ${member} Reason: ${reason} Mod: ${message.author}`)
+message.channel.send(`User banned!`)
+    
+  }
+}
