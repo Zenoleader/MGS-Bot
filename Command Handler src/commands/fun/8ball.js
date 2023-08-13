922d703f9d8832d22ffe75fb4886fa2f9220e1b9
Zeno <119907481+Zenoleader@users.noreply.github.com>
2023-08-13 09:17:19 -0500

    Create 8ball.js

---
+++ Command Handler src/commands/fun/8ball.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js")
+
+module.exports = {
+  name: '8ball',
+  description: 'Get an answer from the 8ball!',
+  async execute(message, args) {
+ const response = args.join('');
+  var choices = [
+    
+        ' **It is certain**',
+        ' **It is decidedly so**',
+        ' **Without a doubt**',
+        ' **Yes definitely**',
+        ' **You may rely on it**',
+        ' **You can count on it**',
+        ' **As I see it, yes**',
+        ' **Most likely**',
+        ' **Outlook good**',
+        ' **Yes**',
+        ' **Signs point to yes**',
+        ' **Absolutely**',
+        ' **Ask again later**',
+        ' **Better not tell you now**',
+        ' **Cannot predict now**',
+        ' **Concentrate and ask again**',
+        " **Don't count on it**",
+        ' **My reply is no**',
+        ' **My sources say no**',
+        ' **Outlook not so good**',
+        ' **Very doubtful**',
+        " **Chances aren't good**"
+        //you can always add more choices!
+    ]
+    
+    let embed = new Discord.MessageEmbed()
+    .setTitle("8ball")
+    .setAuthor("8ball")
+    .addFields(
+      {
+        name: 'Question:',
+        value: response
+      },
+      {
+        name: 'Answer:',
+        value: `${choices[Math.floor(Math.random() * choices.length)]}`
+      }
+    )
+    .setColor("BURPLE")
+    .setTimestamp()
+    message.channel.send({embeds: [embed]})
+  }
+}
