7a76170c39c855d53aca892475f989000d39b1ea
ZenoYT <87985541+ZenoYT@users.noreply.github.com>
2024-01-12 08:28:38 -0600

    Update togglerob.js

--- Command Handler src/commands/economy/togglerob.js
+++ Command Handler src/commands/economy/togglerob.js
@@ -8,7 +8,7 @@ module.exports = {
      if (!message.member.permissions.has("MODERATE_MEMBERS")) return message.channel.send("You don't have perms to use thins!");
   
       const status = db.get(`robCommandStatus_${message.guild.id}`);
-      db.set(`robCommandStatus_${message.guild.id}`, !status)
+      await db.set(`robCommandStatus_${message.guild.id}`, !status)
 
       await message.channel.send(`The rob command has been **${!status ? 'enabled' : 'disabled'}**.`)
     
