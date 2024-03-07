2b84eb1546013938f5ecc86b900c9a52c49570fb
Zeno <119907481+Zenoleader@users.noreply.github.com>
2024-03-07 08:29:05 -0600

    Update togglerob.js

--- Command Handler src/commands/economy/togglerob.js
+++ Command Handler src/commands/economy/togglerob.js
@@ -1,4 +1,6 @@
-const db = require('quick.db')
+const Discord = require('discord.js');
+const { QuickDB } = require("quick.db");
+const db = new QuickDB()
 
 module.exports = {
   name: 'togglerob',
@@ -7,7 +9,7 @@ module.exports = {
 
      if (!message.member.permissions.has("MODERATE_MEMBERS")) return message.channel.send("You don't have perms to use thins!");
   
-      const status = db.get(`robCommandStatus_${message.guild.id}`);
+      const status = await db.get(`robCommandStatus_${message.guild.id}`);
       await db.set(`robCommandStatus_${message.guild.id}`, !status)
 
       await message.channel.send(`The rob command has been **${!status ? 'enabled' : 'disabled'}**.`)
