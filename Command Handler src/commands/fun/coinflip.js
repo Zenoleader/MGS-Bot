8558314f7388c47622c754e3bff3ecca6d845b0e
Zeno <119907481+Zenoleader@users.noreply.github.com>
2024-03-24 16:16:22 -0500

    Update coinflip.js

--- Command Handler src/commands/fun/coinflip.js
+++ Command Handler src/commands/fun/coinflip.js
@@ -1,14 +1,17 @@
-
 module.exports = {
   name: 'coinflip',
-  description: 'Toggle the rob command!',
+  description: 'Flip a coin!',
   async execute(message, args) {
-    const response = args.join('');  
-
+ 
+  const response = args.join('');  
   var coinres = [
     'Heads',
     'Tails'
   ]
+
+    if(!coinres.includes(response)) {
+    return message.channel.send("Please choose a valid side: Heads or Tails.")
+    }
 
   message.channel.send(`You chose ${response}. You got ${coinres[Math.floor(Math.random() * coinres.length)]}`)
   }
