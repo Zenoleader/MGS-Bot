9907186851caec98c108457e6f48c65f7594a12c
Zeno <119907481+Zenoleader@users.noreply.github.com>
2024-03-26 10:05:20 -0500

    Update ping.js

--- Command Handler src/commands/utility/ping.js
+++ Command Handler src/commands/utility/ping.js
@@ -2,8 +2,6 @@ module.exports = {
   name: 'ping',
   description: 'Get the ping for the bot!',
   execute(message) {
-
-message.reply(`Pong! API: ${client.ws.ping}ms`)
-    
+message.channel.send(`🏓 Pong! API: ${client.ws.ping}ms`)    
   }
 }
