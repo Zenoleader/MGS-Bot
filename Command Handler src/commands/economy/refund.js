93b38bc7397191c5d43090320415f3d19dbd71ac
Zeno <119907481+Zenoleader@users.noreply.github.com>
2024-03-26 10:17:04 -0500

    Update refund.js

--- Command Handler src/commands/economy/refund.js
+++ Command Handler src/commands/economy/refund.js
@@ -9,11 +9,11 @@ module.exports = {
 
     if(message.author.id === 'Your ID here') {
 
-   const [recipientMention, amountString] = message.content.trim().split(/\s+/).slice(1);
+      const [recipientMention, amountString] = message.content.trim().split(/\s+/).slice(1);
       const amount = parseInt(amountString);
       
       if (!recipientMention || !amount) {
-        return message.channel.send("Please specift a recipient and an amount to refund!");
+        return message.channel.send("Please specify a recipient and an amount to refund!");
       }
       
       if (amount < 1) {
@@ -22,20 +22,13 @@ module.exports = {
       
       const recipientId = recipientMention.replace(/\D/g, '');
       const recipient = await message.client.users.fetch(recipientId);
-  
-  if(recipientId === message.author.id) {
-    return message.channel.send("You can't refund yourself!");
-  }
-      
       
       await db.add(`balance_${recipient.id}`, amount);
-      const format = amount.toLocaleString()
-      message.client.channels.cache.get("Your log ID").send({content: `Refunder: ${message.author.tag} | ${message.author.id} **${message.author.tag}** has refunded ${recipient.tag} **${format}** Moolah!`})
-  
-await message.channel.send(`**${message.author.tag}** has refunded ${recipient.tag} **${format}** Moolah!`);
+      message.client.channels.cache.get("Your log ID").send({content: `Refunder: ${message.author.tag} | ${message.author.id} **${message.author.tag}** has refunded ${recipient.tag} **${amount.toLocaleString()}** Moolah!`}) 
+      await message.channel.send(`**${message.author.username}** has refunded ${recipient.username} **${amount.toLocalestring()}** Moolah!`);
         
       } else {
-  await message.channel.send("Only the devs can use this!")
+      await message.channel.send("Only the developers can use this!")
     }
- }
+  }
 }
