eedb0bf2a98d928ee1106981d7acf8995a8191b6
Zeno <119907481+Zenoleader@users.noreply.github.com>
2024-03-29 10:06:50 -0500

    Update ping.js

--- Command Handler src/commands/utility/ping.js
+++ Command Handler src/commands/utility/ping.js
@@ -2,6 +2,11 @@ module.exports = {
   name: 'ping',
   description: 'Get the ping for the bot!',
   execute(message) {
-message.channel.send(`🏓 Pong! API: ${client.ws.ping}ms`)    
+    
+ await message.channel.send("Calculating...").then((msg) => {
+      const pingpong = msg.createdTimestamp - message.createdTimestamp
+      msg.edit({ content: `Calculated: **Bot Latency:** ${pingpong}ms **API Latency:** ${message.client.ws.ping}ms`})
+    })
+    
   }
 }
