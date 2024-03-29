b9e1a8a76f313405422a603094a4df530e9d16af
Zeno <119907481+Zenoleader@users.noreply.github.com>
2024-03-29 10:03:27 -0500

    Update report.js

--- Command Handler src/commands/moderation/report.js
+++ Command Handler src/commands/moderation/report.js
@@ -1,11 +1,24 @@
+const Discord = require('discord.js')
+
 module.exports = {
   name: 'report',
   description: 'Report a user or bug!',
   async execute(message, args) {
+
+  if(args < 1)  {
+  return message.channel.send("Please supply a report!")
+  }
     
   const response = args.join('');
-  message.client.channels.cache.get('CHANNEL_ID HERE').send(response+" :USER ID-"+message.author.id+" :SERVER ID-"+message.guild.id)
-  message.channel.send('Sent!')
+
+  let embed = new Discord.MessageEmbed()
+  .setTitle(`${message.author.username} has submitted a report!`)
+  .addFields(
+    { name: 'REPORT', value: `**${response}**`}, {  name: 'USERNAME & ID', value: `${message.author.username} [] ${message.author.id}` }, { name: "GUILD NAME & ID", value: `${message.guild.name} [] ${message.guild.id}`}
+  )
+    
+  await message.client.channels.cache.get('CHANNEL_ID HERE').send({embeds: [embed]})
+  await message.channel.send('Your report has been submitted!')
     
   }
 }
